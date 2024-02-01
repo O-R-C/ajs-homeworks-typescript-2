99f06bffcc57b420724389640048f8aee76fb618
Andrey Semenov <semenov.orc@gmail.com>
2024-02-01 14:52:32 +0300

    add domain to ignore coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['Interfaces/*.{ts}'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/ts/domain/'],
 };
 
